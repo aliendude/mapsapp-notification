94f24543311f2c96b2413f7aa5b76188270b379c
root <pjrodriguezg@unal.edu.co>
2017-03-29 09:30:54 -0400

    Changing port to 7171

--- chat_server.js
+++ chat_server.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
-var port = process.env.PORT || 8000;
-
+var port = process.env.PORT || 7171;
 server.listen(port, function () {
   console.log('Server listening at port %d', port);
 });
